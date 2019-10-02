f9c91fa4ff81012e207eea89a2efac99fd5595bf
Vineesh K P <vineeshkp7@gmail.com>
2019-10-02 14:50:53 +0530

    Create cpanel_accinfo.sh
    Added script to find the domains under each cPanel users.

---
+++ cpanel_accinfo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##Defining color codes
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+cyn=$'\e[1;36m'
+white=$'\e[0m'
+
+##Getting user list
+users=(`cat /etc/trueuserowners | awk '{print $1}' | tr -d :`)
+
+##Getting domain info of all users
+for i in "${users[@]}"; do
+echo "$red ####################################### $white"
+echo "$grn ##Main domain of user ${i}"
+echo "=============="
+cat /etc/userdatadomains | grep mwalters | grep main | awk '{print $1}' | tr -d :
+echo "$cyn ##Addon domains of user ${i}"
+echo "=============="
+cat /etc/userdatadomains | grep mwalters | grep addon | awk '{print $1}' | tr -d :
+echo "$blu ##Subdomains of user ${i}"
+echo "=============="
+cat /etc/userdatadomains | grep mwalters | grep sub | awk '{print $1}' | tr -d :
+done
+echo "$white Thank you"
